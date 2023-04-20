de95a8c7850ca1ea72a332ad18bd32390d04e330
Rauch Rodrigues <94990103+rauchrods@users.noreply.github.com>
2023-04-20 17:36:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,27 @@
 //your code here!
 
+let list = document.querySelector("#infi-list");
+
+let counter = 1;
+
+for (let i = 1; i <= 10; i++) {  
+    appendlist(); 
+}
+
+
+list.addEventListener('scroll', event => {
+    const {scrollHeight, scrollTop, clientHeight} = event.target;
+
+    if (Math.abs(scrollHeight - clientHeight - scrollTop) < 1) {
+        appendlist();
+        appendlist();
+    }
+});
+
+
+function appendlist(){
+      let li = document.createElement("li");
+        li.innerText = `Item ${counter}`;
+        counter++;
+        list.appendChild(li);
+}
